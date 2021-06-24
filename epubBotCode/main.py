db50f37652dc78d2d930ca8c889744cc0416ffc7
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2021-06-24 08:25:20 +0200

    it works

--- epubBotCode/main.py
+++ epubBotCode/main.py
@@ -4,7 +4,13 @@ import json
 import os
 import re
 import shlex
+import sys
+import urllib
 from typing import Dict, Any, Tuple, List, Union, Generator
+import logging
+import boto3
+
+version = '1.0'
 
 from ebooklib import epub
 import praw
@@ -15,6 +21,7 @@ from smart_open import smart_open
 
 
 def main():
+    logging.getLogger().setLevel(logging.INFO)
     reddit_instance = create_reddit_instance()
     triggers = get_trigger_comments(reddit_instance)
     for comment in triggers:
@@ -22,6 +29,7 @@ def main():
 
 
 def create_epub(comment: praw.reddit.Comment, reddit_instance: praw.reddit.Reddit):
+    logger = logging.getLogger('create_epub')
     # noinspection PyBroadException
     try:
         args = command_argparse(comment.body)
@@ -29,6 +37,7 @@ def create_epub(comment: praw.reddit.Comment, reddit_instance: praw.reddit.Reddi
         return
     except:
         comment.reply(create_arg_parser().format_help())
+        logger.info(f'Invalid command {comment.body}')
         return
 
     submission = comment.submission
@@ -38,9 +47,10 @@ def create_epub(comment: praw.reddit.Comment, reddit_instance: praw.reddit.Reddi
     soup = BeautifulSoup(submission_html, features='lxml')
     first_link = soup.find('a', text=re.compile(re.escape(args.first), re.IGNORECASE))
     post_num = 1
-    title = submission.title
+    title: str = submission.title
     if first_link is not None:
         submission = reddit_instance.submission(url=first_link.attrs['href'])
+        logger.info(f'Found first post in chain: {submission.title}')
     else:
         previous_link = soup.find('a', text=re.compile(re.escape(args.previous), re.IGNORECASE))
         while previous_link is not None:
@@ -48,11 +58,12 @@ def create_epub(comment: praw.reddit.Comment, reddit_instance: praw.reddit.Reddi
             submission_html = markdown.markdown(submission.selftext)
             soup = BeautifulSoup(submission_html, features='lxml')
             soup.find('a', text=re.compile(re.escape(args.previous), re.IGNORECASE))
+        logger.info(f'Walked previous links to first in chain: {submission.title}')
 
     set_metadata(book, submission, args)
 
     for submission in yield_submissions(reddit_instance, submission, args.next):
-        print(f'Getting post {post_num}')
+        logging.debug(f'Getting post {post_num}')
         post_num += 1
         if args.max_chapters is not None and post_num > args.max_chapters:
             break
@@ -82,34 +93,35 @@ def create_epub(comment: praw.reddit.Comment, reddit_instance: praw.reddit.Reddi
     design(book, chapters)
     # noinspection PyBroadException
     try:
-        epub.write_epub(f'{title}.epub', book, {})
+        logging.info(f'Creating epub for {title}')
+        epub.write_epub(f'/tmp/{title}.epub', book, {})
         upload_to_s3(f'{title}.epub')
-    except:
+        comment.reply(f'https://reyem-epub-bot-output.s3.us-east-2.amazonaws.com/'
+                      f'{urllib.parse.quote(title).replace("%20", "+")}.epub')
+    except Exception as ex:
+        logging.info(f'Failed to create to epub. Informing requesting user. Exception was {str(ex)}')
         comment.reply('Failed to create epub. Maybe one of your parameters is set wrong? Common mistakes are empty '
                       'chapters')
 
 
-def lambda_handler(event, context):
-    main()
-
-
-if __name__ == '__main__':
-    main()
-
-
 def upload_to_s3(filename: str):
+    logger = logging.getLogger('aws')
+    logger.info(f'Uploading {filename} to aws bucket')
     with smart_open(f's3://reyem-epub-bot-output/{filename}', 'wb') as fout:
-        with smart_open(filename, 'rb') as fin:
+        with smart_open(f'/tmp/{filename}', 'rb') as fin:
             for line in fin:
                 fout.write(line)
+    logger.debug(f'Finished uploading {filename} to aws bucket')
 
 
 def get_config_from_s3() -> Dict[Any, Any]:
+    logger = logging.getLogger('aws')
+    logger.debug('Read config from s3 bucket')
     text = ''
     with smart_open('s3://reyem-epub-bot-config/config.json', 'rb') as s3_file:
         for line in s3_file:
             text += line.decode('utf8')
-
+    logger.debug('Finished reading config from s3 bucket')
     return json.loads(text)
 
 
@@ -136,9 +148,12 @@ def set_metadata(book: epub.EpubBook, submission: reddit.Submission, args: argpa
 
 
 def command_argparse(command: str) -> argparse.Namespace:
+    logger = logging.getLogger('argparse')
+    logger.debug(f'Parsing command {command}')
     parser = create_arg_parser()
     argv = shlex.split(command)
-    if argv[0] != 'u/epuBot':
+    if argv[0] != 'u/epubBot':
+        logger.info('Found an errant mention')
         raise ValueError('Not a invocation')
     return parser.parse_args(argv[1:])
 
@@ -176,37 +191,52 @@ def create_arg_parser() -> argparse.ArgumentParser:
     return parser
 
 
-version = '0.3'
-
-
 def create_reddit_instance() -> praw.reddit.Reddit:
-    print(os.getcwd())
+    logger = logging.getLogger('reddit')
     config = get_config_from_s3()
 
-    return praw.Reddit(
+    reddit_instance = praw.Reddit(
         client_id=config['client_id'],
         client_secret=config['client_secret'],
         username='epubBot',
         password=config['password'],
         user_agent=f'linux:de.reyem.epubBot:{version} (by /u/epubBot)'
     )
+    logger.info('Authorised to reddit')
+    return reddit_instance
 
 
 def get_trigger_comments(reddit_instance: praw.Reddit) -> Generator[praw.reddit.Comment, None, None]:
-    generator = reddit_instance.inbox.unread()
+    logger = logging.getLogger('reddit')
+    generator = reddit_instance.inbox.mentions()
     for comment in generator:
-        if comment is praw.reddit.Comment:
+        logger.debug(f'Found comment {comment.fullname}')
+        if comment.new:
+            logger.info(f'Found trigger comment {comment.fullname}')
             comment.mark_read()
             yield comment
+        else:
+            logger.debug(f'Discarding comment {comment.fullname} as read')
 
 
 def yield_submissions(reddit_instance: praw.reddit.Reddit, submission: praw.reddit.Submission, next_pattern: str) \
         -> Generator[praw.reddit.Submission, None, None]:
+    logger = logging.getLogger('reddit')
+    logger.info(f'Returning submission {submission.title}')
     yield submission
     soup = BeautifulSoup(markdown.markdown(submission.selftext), features='lxml')
     next_link = soup.find('a', text=re.compile(re.escape(next_pattern), re.IGNORECASE))
     while next_link is not None:
         submission = reddit_instance.submission(url=next_link.attrs['href'])
+        logger.info(f'Returning submission {submission.title}')
         yield submission
         soup = BeautifulSoup(markdown.markdown(submission.selftext), features='lxml')
         next_link = soup.find('a', text=re.compile(re.escape(next_pattern), re.IGNORECASE))
+
+
+def lambda_handler(event, context):
+    main()
+
+
+if __name__ == '__main__':
+    main()
